92b087c54b7c004cb11b1f6c38c5dcedb011d768
Raymond <rs01247@gmail.com>
2018-09-06 19:11:13 -0700

    added simpsons card component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
+import Simp from "./Simp";
 import Header from "./Header";
 import Container from "./Container";
 import simpsons from "../simpsons.json";
 
-class Game extends Component {
+class Game extends React.Component {
+
     state = {
         simpsons,
         score: 0,
         topScore: 0,
     }
 
-    componentDidMount() {
-
+    gameOver = () => {
+        if (this.state.score > this.state.topScore) {
+            this.setState({ topScore: this.state.score }, () => console.log(this.state.topScore));
+        }
+        this.state.simpsons.each(simpsons => {
+            simpsons.clicked = false;
+        });
+        this.setState({ score: 0 });
+        return true;
     }
+
+
+
+    componentDidMount() {
+        // this.setState({ simpsons: this.clickCount(this.state.simpsons) });
+      }
+    
 
     render() {
         return (
             <div>
                 <Header />
-                <Container />
+                <Container score={this.state.score} topScore={this.state.topScore}>
+                    {this.state.simpsons.map(simpsons => (
+                        <Simp
+                            clickCount={this.clickCount}
+                            id={simpsons.id}
+                            key={simpsons.id}
+                            image={simpsons.image}
+                        />
+                    ))}
+                </Container>
             </div>
         )
     }
---
+++ src/components/Simp/Simp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Simp.css"
+
+const Simp = props => {
+    <div className="simp-card" onClick={() => props.clickCount(props.id)}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+};
+
+export default Simp;
